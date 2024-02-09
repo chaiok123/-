602d5ea57cf58d858278bcae8455bbfb87a7e317
chaiok123 <158596915+chaiok123@users.noreply.github.com>
2024-02-09 19:21:32 +0700

    скрипт номер 1

---
+++ ScriptNomberOne.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptNomberOne : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //сообщение дл€ потомков
+        
+        print("hello world my name is Denis") ;
+        print("ѕривет мир € ƒенис");
+        print("€ оставл€ю это сообщение в командной строке юнити");
+        print("у мен€ всЄ хорошо");
+        print("а у теб€?€ знаю что не смогу тебе ответить так как мы общаемс€ не в р€льном времени а € отправл€ю тебе заскриптованные сообщени€ так что желаю тебе всего наилучшего");
+        print("конец св€зи(нет,€ прождолжу остовл€ть сообщени€ в своих других скриптах(:");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
